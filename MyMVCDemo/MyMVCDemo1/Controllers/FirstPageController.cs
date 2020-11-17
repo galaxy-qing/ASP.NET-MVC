5a6989858e665a746e87e9e4c5113a13292a7e8d
galaxy-qing <44334049+galaxy-qing@users.noreply.github.com>
2020-11-17 23:35:21 +0800

    添加demo

---
+++ MyMVCDemo/MyMVCDemo1/Controllers/FirstPageController.cs
@@ -0,0 +1,37 @@
+﻿using MyMVCDemo1.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMVCDemo1.Controllers
+{
+    public class FirstPageController : Controller
+    {
+        // GET: FirstPage
+        public ActionResult Index()
+        {
+
+            Student stu = new Student()
+            {
+                Id = 1,
+                Age = 16,
+                Name="张三",
+            };
+
+
+            return View(stu);
+        }
+
+        public ActionResult Page1(string para)
+        {
+            return View();
+        }
+
+        public ActionResult Page2(string para)
+        {
+            return View();
+        }
+    }
+}
